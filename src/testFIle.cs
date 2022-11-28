2f14fc7cd365a99ced9425e3791355ffe27844ec
TheFastCreeper <andreasmiller@gmx.ch>
2022-11-28 08:40:16 +0100

    Added exit code

--- src/testFIle.cs
+++ src/testFIle.cs
@@ -1,9 +1,10 @@
 using System;
 
 public class TestClass {
-    public static void Main(String [] args) {
+    public static int Main(String [] args) {
         for (int a = 0;a<100;a++) {
         Console.WriteLine("Hello World! Iteration : "+a);
         }
+        return 0;
     }
 }
