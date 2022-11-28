0e0770c3fd04e6f85af46ce8ba8ac3c53cd601d4
TheFastCreeper <andreasmiller@gmx.ch>
2022-11-28 08:39:03 +0100

    Uh yeah so too many files

---
+++ src/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class testClass {
+
+    public static int Main () {
+Console.WriteLine("Hello, World!");
+return 0;
+    }
+}
--- src/testFIle.cs
+++ src/testFIle.cs
@@ -2,14 +2,8 @@ using System;
 
 public class TestClass {
     public static void Main(String [] args) {
-        Console.WriteLine("Hello World!");
-        pointerTest();
-    }
-
-    public unsafe static void pointerTest() {
-        int a = 5;
-        int* b = &a;
-        Console.WriteLine("0x"+b);
-
+        for (int a = 0;a<100;a++) {
+        Console.WriteLine("Hello World! Iteration : "+a);
+        }
     }
 }
