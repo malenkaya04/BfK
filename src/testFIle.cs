4359586adf283c6136d1b11a6a5849249b91cf88
TheFastCreeper <andreasmiller@gmx.ch>
2022-11-28 08:25:37 +0100

    Added pointer test for vsc

--- src/testFIle.cs
+++ src/testFIle.cs
@@ -3,5 +3,13 @@ using System;
 public class TestClass {
     public static void Main(String [] args) {
         Console.WriteLine("Hello World!");
+        pointerTest();
+    }
+
+    public unsafe static void pointerTest() {
+        int a = 5;
+        int* b = &a;
+        Console.WriteLine("0x"+b);
+
     }
 }
