efb50d8e27defe818d0b1898bb11ce73674258ef
malenkaya04 <andreasmiller@gmx.ch>
2022-11-29 19:09:20 +0100

    Deleted unnecessary file

--- src/helloWorld.cs
+++
@@ -1,9 +0,0 @@
-//This is the repository base, branch from the master for certain tasks
-//After excercise is done, merge them back into master in a seperate folder
-//Modify only if necessary!
-using System;
-public class testClass {
-	public static void Main(String[] args) {
-		
-	}
-}
