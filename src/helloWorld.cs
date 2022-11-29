a2e0bf63f158d9452f9c9c39d6c35e9b3319801a
malenkaya04 <andreasmiller@gmx.ch>
2022-11-29 10:13:55 +0100

    Added description for main class

--- src/helloWorld.cs
+++ src/helloWorld.cs
@@ -1,3 +1,5 @@
+//This is the repository base, branch from the master for certain tasks
+//Modify only if necessary!
 using System;
 public class testClass {
 	public static void Main(String[] args) {
