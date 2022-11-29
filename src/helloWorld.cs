a0a2dc022f25939502afe7c98576c17fcfb60817
TheFastCreeper <andreasmiller@gmx.ch>
2022-11-29 09:24:03 +0100

    Removed testing comments

--- src/helloWorld.cs
+++ src/helloWorld.cs
@@ -2,14 +2,6 @@ using System;
 public class testClass {
 	public static void Main(String[] args) {
 		Console.WriteLine("Hello World!");
-		//Random comment
-		//Second comment
-		//Third line
-<<<<<<< HEAD
-		//Lorem Ipsum Dolor Sit Amet
-=======
-		//4th line
-		//5th line
->>>>>>> vscode
+		
 	}
 }
