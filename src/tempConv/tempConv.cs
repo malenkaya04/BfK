0ca81676e5b559d28025060a57348ccaad01b37c
malenkaya04 <andreasmiller@gmx.ch>
2022-11-30 11:55:07 +0100

    Added comments for tempConv

--- src/tempConv/tempConv.cs
+++ src/tempConv/tempConv.cs
@@ -1,11 +1,14 @@
 using System;
 public class converterToF {
     public static void Main () {
+        //Prompting input from user
         Console.WriteLine("Hello! Please enter a temperature!");
+        //Declare variable and initialize with read-in value from terminal. Gets converted to double
         var tempInC = Convert.ToDouble(Console.ReadLine());
+        //Creating output, calls seperate function to calculate the final value which gets appended to output
         Console.WriteLine("Your temeperature is : " +Convert.ToString(convert(tempInC)));
     }
-
+    //Seperate function to do the conversion
     public static double convert(double tempInC) {
         return ((tempInC * 1.8) +32);
     }
