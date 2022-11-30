a8609ed355f822b0634e82f11864b393a23395d2
malenkaya04 <andreasmiller@gmx.ch>
2022-11-30 12:03:54 +0100

    Added comments for oilDelivery

--- src/oilDelivery/oilDelivery.cs
+++ src/oilDelivery/oilDelivery.cs
@@ -3,6 +3,7 @@ public class converterToF {
     public static void Main () {
         try {
         //Asking for customer data
+        //Declaring the variables and initializing them with converted user input
         Console.WriteLine("Willkommen bei OilDelivery GmbH!");
         Console.WriteLine("Bitte geben sie ihren Namen ein!");
         var customerName = Console.ReadLine();
@@ -13,6 +14,7 @@ public class converterToF {
         Console.WriteLine("Wie war der Tankstand des Lieferfahrzeugs nach der Lieferung?");
         var amountAfter = Convert.ToDouble(Console.ReadLine());
         //Validating customer data
+        //If any data is not correctly formated, notify user and restart program
         if (amountAfter > amountBefore || customerName == "" || oilPrice < 0) {
             Console.WriteLine("Bitte überprüfen sie ihre Angaben und versuchen sie es erneut!");
             Main(); 
@@ -27,8 +29,9 @@ public class converterToF {
             Main();
         }
     }
-
+    //helper function to for formated output
     public static void formatedOutput(String customerName, double amountDelivered, double oilPrice) {
+        //Final values get calculated from params and constant for sales tax
         Console.WriteLine("---- Heizölrechnung ----");
         Console.WriteLine("Kundenname : "+customerName);
         Console.WriteLine("Gelieferte Menge : " +amountDelivered+" Liter");
