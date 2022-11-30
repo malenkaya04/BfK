d01261607b66c0491a32757e75c72a5aad6f1a59
malenkaya04 <andreasmiller@gmx.ch>
2022-11-30 08:00:33 +0100

    Added comments

--- src/imageCalc/imageCalc.cs
+++ src/imageCalc/imageCalc.cs
@@ -1,11 +1,13 @@
 using System;
 public class imageCalc{
     public static void Main(String[] args) {
+        //Array solution is legacy; Needs refactoring
        String[] colorDepth = new String[2];
         Console.WriteLine("Please ente a name for the color depth!");
         colorDepth[0] = Console.ReadLine();
         Console.WriteLine("Please enter a color depth!");
         colorDepth[1] = Console.ReadLine();
+        //Creating some more variables to waste more RAM
        float imageSizeBit = imageSizeCalc(Convert.ToInt16(colorDepth[1]));
        float imageSizeByte = imageSizeBit/8;
        float imageSizeKB = imageSizeByte/1024;
