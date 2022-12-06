49cb829d25a7e2d0d5de2093ff2c4f31ab78c378
malenkaya04 <andreasmiller@gmx.ch>
2022-12-06 11:54:37 +0100

    Updated carSales2 and executable

--- src/carSales/carSales.cs
+++ src/carSales/carSales.cs
@@ -1,6 +1,22 @@
 using System;
 public class carSales {
 	public static void Main() {
-		Console.WriteLine("Hello World");
+		string employeeName;
+		double salesNumber;
+
+		Console.WriteLine("Please enter a employee name");
+		employeeName = Console.ReadLine();
+
+		Console.WriteLine("Please enter the sales of employee " + employeeName);
+		salesNumber = Convert.ToDouble(Console.ReadLine());
+
+		if (salesNumber >= 200000) {
+			Console.WriteLine("Employee is eligble for the bonus");
+			Console.WriteLine("The bonus will be " +salesNumber*0.0235 +"$");
+	}
+		else {
+			Console.WriteLine("Employee is not eligble for the bonus");
+		}
 	}
 }
+
